41b3e8450c0c1a13af11a1f627ab0c02fad6317b
Côme Arvis <come.arvis@neuronest.net>
2021-02-07 23:29:51 +0100

    (config) Add data retrieving into installation script

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ conda create --name gan python=3.8
 conda activate gan
 
 pip install --upgrade pip
+pip install gdown
+gdown https://drive.google.com/uc?id=1vgKReklsHDXRzNokWjUVkd-EIox6_f9_ -O /tmp/
+unzip /tmp/summer2winter_yosemite.zip -d data
+rm summer2winter_yosemite.zip
 pip install -Ur requirements.txt
 pip install -Ur requirements_dev.txt
 
